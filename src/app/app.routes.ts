3ced02691fb2c4a11739d287d73fe37eac1767a8
davidclimbing <davidclimbing@github.com>
2025-01-06 15:04:25 +0900

    init components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import {AboutComponent} from "./components/about/about.component";
+import {ProjectsComponent} from "./components/projects/projects.component";
+import {PageNotFoundPageComponent} from "./components/page-not-found-page/page-not-found-page.component";
+import {HomeComponent} from "./components/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+    },
+    {
+        path: 'projects',
+        component: ProjectsComponent,
+    },
+    {
+        path: '**',
+        redirectTo: 'page-not-found'
+    },
+    {
+        path: 'page-not-found',
+        component: PageNotFoundPageComponent
+    }
+];
