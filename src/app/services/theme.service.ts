5f2fd19c7771ee45515e76dd8aaf843c67577774
David Oh <general.davidclimbing@gmail.com>
2025-02-09 22:09:16 +0900

    styling theme

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,6 +1,5 @@
 import {Injectable, signal, WritableSignal} from '@angular/core';
 import { THEME } from '../schemas/ui';
-import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +7,10 @@ import { BehaviorSubject } from 'rxjs';
 export class ThemeService {
   defaultTheme: string = THEME.DARK;
   themeSignal: WritableSignal<string> = signal(this.defaultTheme);
-  themeSubject: BehaviorSubject<string> = new BehaviorSubject<string>(this.defaultTheme);
 
-  constructor() { }
+  constructor() {
+    this.defaultTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : this.defaultTheme;
+  }
 
   themeInitialize() {
     document.body.classList.forEach(cls => {
@@ -24,14 +24,14 @@ export class ThemeService {
   }
 
   setTheme(theme: string) {
-    theme ? this.defaultTheme = theme : this.defaultTheme;
+    this.themeSignal.update(() => theme ? theme : this.defaultTheme);
     document.body.classList.forEach(cls => {
       if (cls.startsWith('theme')) {
         document.body.classList.remove(cls);
       }
     })
 
-    document.body.classList.add(`theme-${this.defaultTheme}`);
-    localStorage.setItem('theme', this.defaultTheme);
+    document.body.classList.add(`theme-${this.themeSignal()}`);
+    localStorage.setItem('theme', this.themeSignal());
   }
 }
